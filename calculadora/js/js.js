dd8e4187a6e375f0e9048e55ab60719c529f3126
xavirafa <77781618+xavirafa@users.noreply.github.com>
2021-02-12 07:11:12 -0500

    Add files via upload

--- calculadora/js/js.js
+++ calculadora/js/js.js
@@ -5,23 +5,23 @@ function limpiar() {
 function sumar(){
 	var x = parseInt(document.getElementById("valor1").value);
 	var y = parseInt(document.getElementById("valor2").value);
-	document.getElementById("resultado").innerHTML = x+y;
+	document.getElementById("resultado").value = x+y;
 }
 
 function restar(){
 	var x = parseInt(document.getElementById("valor1").value);
 	var y = parseInt(document.getElementById("valor2").value);
-	document.getElementById("resultado").innerHTML = x-y;
+	document.getElementById("resultado").value = x-y;
 }
 
 function multiplicar(){
 	var x = parseInt(document.getElementById("valor1").value);
 	var y = parseInt(document.getElementById("valor2").value);
-	document.getElementById("resultado").innerHTML = x*y;
+	document.getElementById("resultado").value = x*y;
 }
 
 function dividir(){
 	var x = parseInt(document.getElementById("valor1").value);
 	var y = parseInt(document.getElementById("valor2").value);
-	document.getElementById("resultado").innerHTML = x/y;
+	document.getElementById("resultado").value = x/y;
 }
